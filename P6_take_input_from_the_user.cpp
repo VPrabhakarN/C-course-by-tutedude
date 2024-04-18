9a891301e4b7b606460c8f3ab64d5617c5b17be3
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-18 19:49:56 +0530

    Add files via upload
    It's a new module in which I learned how to take input from users in C++ programming language. It's my first program in C++ in which I wrote the code to take input from the users.

---
+++ P6_take_input_from_the_user.cpp
@@ -0,0 +1,15 @@
+// C++ program to take input from the user 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+    cout << "You age is ... " << age;
+
+    return 0;
+}
