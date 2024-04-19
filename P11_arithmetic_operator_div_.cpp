a126d9bbe41701283f53379a755a2ef8189f9864
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-19 12:57:31 +0530

    Add files via upload
    C++ program to define arithmetic operator '/ '.

---
+++ P11_arithmetic_operator_div_.cpp
@@ -0,0 +1,18 @@
+// C++ program to define arithmetic operator '/'
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a = 1000;
+    b = 250;
+
+    // '/' arithmetic operator is used to perform division
+    c = a / b; 
+    cout << "Division of a & b : " << c << endl;
+
+    return 0;
+}
