ccb98daf512b75b93f50d11e143ea13aea142cd5
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-19 12:56:55 +0530

    Add files via upload
    c++ program to define arithmetic operator '*'.

---
+++ P10_arithmetic_operator_mul_.cpp
@@ -0,0 +1,18 @@
+// C++ program to define arithmetic operator '*'
+
+#include<iostream> 
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a = 10;
+    b = 30;
+
+    // '*' arithmetic operator is used to perform multiplication
+    c = a * b;
+    cout << "The multiplication of a & b : " << c << endl;
+    
+    return 0;
+}
