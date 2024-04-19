7f248ab3c8e3abbecd3dd8638d2e6c977978e2fb
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-19 12:48:13 +0530

    Add files via upload
    C++ Program to define an arithmetic operator '+'.

---
+++ P8_arithmetic_operators_add_.cpp
@@ -0,0 +1,18 @@
+// C++ program to define arithmetic operator '+' 
+
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a, b, c;
+    a = 1000;
+    b = 250;
+
+    // '+' operator is used for addition 
+    c = a + b;
+    cout << "Addition of a & b : " << c << endl;
+
+    return 0;
+}
