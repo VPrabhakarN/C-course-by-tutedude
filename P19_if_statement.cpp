5d6d3e4bc65f8ada3c57dd85a48e6fde48196850
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-20 09:26:48 +0530

    Add files via upload
    I started a new module of the course. C++ program to define if condition.

---
+++ P19_if_statement.cpp
@@ -0,0 +1,16 @@
+// C++ program to define 'if statement'
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int year = 2003;
+
+    if(year > 2000)
+    {
+        cout << "Wecome to the 21st century!";
+    }
+    return 0;
+}
