fbb5d6ac4e3d39db0fc57d4bf2758b26660bc403
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-22 18:22:46 +0530

    Add files via upload
    C++ program to demonstrate while loop.

---
+++ P29_while_loop.cpp
@@ -0,0 +1,20 @@
+// C++ program to demonstrate 'while loop'
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"It is an example of while loop" << endl;
+    int a;
+    a = 0;
+
+    // using while loop to print 0-9 numbers
+    while (a < 10){
+        cout << a << endl;
+        a++;
+    }
+
+    return 0;
+}
