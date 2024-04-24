5c4382e74b9838f2019b70636c649cbc34fc0a95
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-24 15:18:12 +0530

    Add files via upload

---
+++ P46_char_array_as_string_using_array.cpp
@@ -0,0 +1,20 @@
+// C++ program to demostrate char array for string
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    // here define char array to get string 
+    char arr[6] = "Vijay";
+
+    // traversal array 
+    for(int i=0; i<5; i++)
+    {
+        cout << arr[i];
+    }
+
+    return 0;
+}
