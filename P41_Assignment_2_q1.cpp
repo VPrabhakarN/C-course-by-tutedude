55418b02212bfa2a3dc4a24db48440dd7264da53
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-24 15:14:20 +0530

    Add files via upload

---
+++ P41_Assignment_2_q1.cpp
@@ -0,0 +1,19 @@
+//  Write a program in C++ to find the first 10 natural numbers.
+// Sample output: The natural numbers are:1 2 3 4 5 6 7 8 9 10
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // printing natural numbers using for loop 
+    // as we know N = {1,2,3,4,5.....n};
+
+    for(int i=1; i<=10; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
