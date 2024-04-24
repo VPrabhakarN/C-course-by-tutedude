768eda2052e46d7b14ed926a28504409e9f7d0b0
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-24 15:20:07 +0530

    Add files via upload

---
+++ P50_char_array_as_string_len().cpp
@@ -0,0 +1,19 @@
+// C++ program to define the length of the string 
+
+#include<iostream>
+#include<cmath>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+    // defining char array as string 
+    char arr[6] = "vijay";
+    cout << arr << endl;
+
+    // length of the string 
+    cout << "Length : " << strlen(arr) << endl;
+
+    return 0;
+}
