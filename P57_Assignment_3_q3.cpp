8c4088073ba345a5519de8625e28271d5b1b86c4
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-25 08:39:20 +0530

    Add files via upload

---
+++ P57_Assignment_3_q3.cpp
@@ -0,0 +1,18 @@
+// Write a program to count a number of vowels and consonants in a String?
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string _input;
+    char vowels[10] = {'a', 'A', 'e','E','i','I', 'o', 'O', 'u', 'U' };
+
+    // taking input from the user 
+    cout << "Enter the string : " ;
+    cin >> _input;
+
+    // 
+}
