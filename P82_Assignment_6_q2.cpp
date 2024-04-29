b22d1a366e0d0372c3f384c23df78ef5cf483d70
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-04-29 23:43:04 +0530

    Add files via upload

---
+++ P82_Assignment_6_q2.cpp
@@ -0,0 +1,38 @@
+// Q.2) Demonstrate operator overloading with the help of an example program
+
+#include<iostream>
+
+using namespace std;
+
+// defining a class
+class Complex {
+    private :
+    int real, imag;
+
+    public :
+    Complex(int r=0, int i=0){
+        real = r;
+        imag = i;
+    }
+
+    // This is automatically called when '+' is used with
+    // between two complex objects
+
+    Complex operator+(Complex const& obj){
+        Complex res;
+        res.real = real + obj.real;
+        res.imag = imag + obj.imag;
+        return res;
+    }
+
+    void print(){
+        cout << real << " + i" << imag << '\n';
+    }
+};
+
+int main(){
+    Complex c1(10,5), c2(2,4);
+    Complex c3 = c1 + c2;
+    c3.print();
+    return 0;
+}
