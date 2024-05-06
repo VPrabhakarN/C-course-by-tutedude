bb03ee655f80a7969c794d5caf353d740cb6dc76
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-06 11:57:46 +0530

    Add files via upload

---
+++ P110_Assignment_8_q1.cpp
@@ -0,0 +1,25 @@
+// Q.1) What is the ouput of the following program 
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Box{
+    int capacity;
+    public :
+    Box(int cap){
+        capacity = cap;
+    }
+    friend void show();
+};
+
+void show(){
+    Box b(10);
+    cout << "Value of capacity is : " << b.capacity << endl;
+}
+
+int main(int argc, char const*argv[]){
+
+    show();
+    return 0;
+}
