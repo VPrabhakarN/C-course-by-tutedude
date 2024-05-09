b3e689bbdd46b047c362257fdde0a87a9e0c7d2d
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-09 22:29:03 +0530

    Add files via upload

---
+++ P116_multiple_catch_blocs.cpp
@@ -0,0 +1,37 @@
+// C++ program to demonstrate the the exception handling 
+#include<iostream>
+using namespace std;
+
+int division(int a, int b);
+
+int main(){
+    int a , b, c;
+
+    // Taking value from the user
+    cout << "Enter the value of a & b respectively : ";
+    cin >> a >> b ;
+
+    // using try block
+    try{
+        c  = division(a, b);
+        cout << c;
+    }
+    catch(int e){
+        cout << e;
+    }
+    catch(float e){
+        cout << e;
+    }
+
+    return 0;
+}
+
+// defining a function
+int division(int a, int b){
+    if(b == 0){
+        throw (float)1.1;
+    }
+    else {
+        return (a/b);
+    }
+}
