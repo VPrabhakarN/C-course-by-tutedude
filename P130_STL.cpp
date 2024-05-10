02a61a1e8e675363b2b763e0e5de7bfa123a0b22
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-10 06:14:11 +0530

    Add files via upload

---
+++ P130_STL.cpp
@@ -0,0 +1,17 @@
+// C++ program to demonstrate the STL (Standard Template Library)
+#include<iostream>
+using namespace std;
+
+// STL - 
+     // algorithms (functions)
+        // merge sort, quik sort, tree sort, heap sort.....
+    // containers (data structures)
+       // vector, list (linked list-doubly), forward lisy, tree, map
+
+    // Iterator
+    // .begin(), .end()..
+
+int main(){
+
+    return 0;
+}
